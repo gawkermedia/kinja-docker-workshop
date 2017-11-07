4c6fe44d682bc35142f4d9b39b502bb0a43e721a
Zoltan Kalmar <kalmiz@gmail.com>
2017-11-07 09:57:42 +0100

    Remove SOURCE_DIR var

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-SOURCE_DIR=$(pwd)
 #PORT_CFG=$(node -e "const c = require('./config'); console.log(\`-p 80:\${c.port} -p 443:\${c.httpsPort}\`);")
-docker run --rm --dns 8.8.8.8 -p 80:8080 -p 443:8443 -v $SOURCE_DIR:/home/node/app -w /home/node/app --name kinja-docker-workshop kinja-docker-workshop $@
+docker run --rm --dns 8.8.8.8 -p 80:8080 -p 443:8443 -v $(pwd):/home/node/app -w /home/node/app --name kinja-docker-workshop kinja-docker-workshop $@
